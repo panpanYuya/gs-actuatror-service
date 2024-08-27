8d232a0397eaed5757e01a7661198eb2b806e340
panpanYuya <yuya@sanbakaraisu.com>
2024-08-27 19:33:37 +0900

    study

---
+++ src/main/java/com/example/gs_actuatror_service/GsActuatrorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gs_actuatror_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsActuatrorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsActuatrorServiceApplication.class, args);
+	}
+
+}
